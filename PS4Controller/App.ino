3f8ea4fdce556a1b98e55463e2b86c9d9bc1c08b
JackChen <15002902506@163.com>
2022-12-18 19:18:22 +0800

    Using PS4Controller
    Update

---
+++ PS4Controller/App.ino
@@ -0,0 +1,35 @@
+﻿
+#include <PS4Controller.h>
+
+void setup() {
+
+	Serial.begin(9600);
+	//Serial.print("Starting...");
+
+	PS4.begin();
+}
+
+unsigned long currentMillis, previousMillis, period = 10;
+uint8_t arr[6] = { 3, 0, 0, 0, 0, 0 };
+
+void loop() {
+
+	currentMillis = millis();
+	if (currentMillis >= previousMillis && currentMillis - previousMillis < period)
+		return;
+	previousMillis = currentMillis;
+
+	int left = PS4.LStickY() * 20;
+	int right = PS4.RStickY() * 20;
+
+	arr[1] = (0xff00 & left) >> 8;
+	arr[2] = 0xff & left;
+
+	arr[3] = (0xff00 & right) >> 8;
+	arr[4] = 0xff & right;
+
+	//CheckSum
+	arr[5] = ~(arr[0] + arr[1] + arr[2] + arr[3] + arr[4]);
+
+	Serial.write(arr, 6);
+}
